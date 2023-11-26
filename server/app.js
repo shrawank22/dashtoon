e654b9e60d8ddeae389997610bcbb270666b77e2
shrawank22 <shrawan8565@gmail.com>
2023-11-26 20:30:00 +0530

    cors

--- server/app.js
+++ server/app.js
@@ -6,12 +6,19 @@ const axios = require('axios');
 
 const app = express();
 
-app.use(cors({
-    origin:[
-        "http://localhost:3000/",
-        "https://dashtoon-comic-generator.onrender.com/"
-    ]
-}));
+const corsOptions ={
+    origin: ['http://localhost:3000', "https://dashtoon-comic-generator.onrender.com/"],
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+
+// app.use(cors({
+//     origin:[
+//         "http://localhost:3000/",
+//         "https://dashtoon-comic-generator.onrender.com/"
+//     ]
+// }));
 app.use(express.json());
 
 const API_ENDPOINT = "https://xdwvg9no7pefghrn.us-east-1.aws.endpoints.huggingface.cloud";
